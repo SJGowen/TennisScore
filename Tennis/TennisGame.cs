5a06c2601a5349a96b7bc1f734174efea1617c32
Stephen Gowen <sjgowen@gmail.com>
2020-02-11 14:11:12 +0000

    Made AwardPointOnFifteenMakesItEqualThirty pass by adding further case to GetTennisScore

--- Tennis/TennisGame.cs
+++ Tennis/TennisGame.cs
@@ -17,6 +17,7 @@ namespace Tennis
         {
             if (score == 0) return "Love";
             if (score == 1) return "Fifteen";
+            if (score == 2) return "Thirty";
             return "Error";
         }
 
