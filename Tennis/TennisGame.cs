1b81905c98f03dfaa43fab146a89e344f6e24044
Stephen Gowen <sjgowen@gmail.com>
2020-02-11 16:16:11 +0000

    Completed TennisGame and TennisGameTests returning '{score} All', 'Deuce', 'Advantage {player name}' and 'Game {player name}'. You can also personalise game by giving the players custom names.

--- Tennis/TennisGame.cs
+++ Tennis/TennisGame.cs
@@ -4,13 +4,52 @@ namespace Tennis
 {
     public class TennisGame
     {
+        private string PlayerOneName { get; set; }
+
+        private string PlayerTwoName { get; set; }
+
         private int PlayerOneScore { get; set; }
 
         private int PlayerTwoScore { get; set; }
 
         public string Score
         {
-            get { return $"{GetTennisScore(PlayerOneScore)}-{GetTennisScore(PlayerTwoScore)}"; }
+            get
+            {
+                if (PlayerOneScore + PlayerTwoScore < 6)
+                {
+                    if (PlayerOneScore != PlayerTwoScore)
+                    {
+                        if (PlayerOneScore == 4 || PlayerTwoScore == 4)
+                        {
+                            return $"Game {GetHighestPlayer()}";
+                        }
+
+                        return $"{GetTennisScore(PlayerOneScore)}-{GetTennisScore(PlayerTwoScore)}";
+                    }
+                    else
+                    {
+                        return $"{GetTennisScore(PlayerOneScore)} All";
+                    }
+                }
+                else if (PlayerOneScore == PlayerTwoScore)
+                {
+                    return "Deuce";
+                }
+                else if (Math.Abs(PlayerOneScore - PlayerTwoScore) == 1)
+                {
+                    return $"Advantage {GetHighestPlayer()}";
+                }
+                return $"Game {GetHighestPlayer()}";
+            }
+        }
+
+        private string GetHighestPlayer()
+        {
+            if (PlayerOneScore > PlayerTwoScore)
+                return $"'{PlayerOneName}'";
+            else
+                return $"'{PlayerTwoName}'";
         }
 
         private string GetTennisScore(int score)
@@ -22,8 +61,10 @@ namespace Tennis
             return "Error";
         }
 
-        public TennisGame()
+        public TennisGame(string playerOne = null, string playerTwo = null)
         {
+            PlayerOneName = string.IsNullOrEmpty(playerOne) ? "Player One" : playerOne;
+            PlayerTwoName = string.IsNullOrEmpty(playerTwo) ? "Player Two" : playerTwo;
         }
 
         public void AwardPoint(bool playerOneScores)
--- TennisTests/TennisGameTests.cs
+++ TennisTests/TennisGameTests.cs
@@ -8,7 +8,7 @@ namespace Tennis
         public void NewGameHasScoreLoveLove()
         {
             var tennisGame = new TennisGame();
-            Assert.Equal("Love-Love", tennisGame.Score);
+            Assert.Equal("Love All", tennisGame.Score);
         }
 
         [Fact]
@@ -36,6 +36,67 @@ namespace Tennis
             tennisGame.AwardPoint(true);
             tennisGame.AwardPoint(true);
             Assert.Equal("Forty-Love", tennisGame.Score);
+        }
+
+        [Fact]
+        public void AwardPointOnFortyMakesItEqualGame()
+        {
+            var tennisGame = new TennisGame();
+            tennisGame.AwardPoint(true);
+            tennisGame.AwardPoint(true);
+            tennisGame.AwardPoint(true);
+            tennisGame.AwardPoint(true);
+            Assert.Equal("Game 'Player One'", tennisGame.Score);
+        }
+
+        [Theory]
+        [InlineData("Love All", 0, 0)]
+        [InlineData("Fifteen All", 1, 1)]
+        [InlineData("Thirty All", 2, 2)]
+        [InlineData("Deuce", 3, 3)]
+        [InlineData("Game 'Player One'", 4, 2)]
+        [InlineData("Advantage 'Player One'", 4, 3)]
+        [InlineData("Game 'Player One'", 5, 3)]
+        [InlineData("Game 'Player Two'", 5, 7)]
+        [InlineData("Advantage 'Player Two'", 6, 7)]
+        [InlineData("Deuce", 8, 8)]
+        public void GetTennisScore(string score, int playerOneScore, int playerTwoScore)
+        {
+            var tennisGame = new TennisGame();
+            for (int i = 0; i < playerOneScore; i++)
+            {
+                tennisGame.AwardPoint(true);
+            }
+
+            for (int i = 0; i < playerTwoScore; i++)
+            {
+                tennisGame.AwardPoint(false);
+            }
+
+            Assert.Equal(score, tennisGame.Score);
+        }
+
+        [Theory]
+        [InlineData("Game 'Borg'", 4, 0)]
+        [InlineData("Game 'Borg'", 4, 1)]
+        [InlineData("Advantage 'Borg'", 4, 3)]
+        [InlineData("Game 'Borg'", 5, 3)]
+        [InlineData("Game 'Agasi'", 5, 7)]
+        [InlineData("Advantage 'Agasi'", 6, 7)]
+        public void GetTennisScoreForNamedPlayers(string score, int playerOneScore, int playerTwoScore)
+        {
+            var tennisGame = new TennisGame("Borg", "Agasi");
+            for (int i = 0; i < playerOneScore; i++)
+            {
+                tennisGame.AwardPoint(true);
+            }
+
+            for (int i = 0; i < playerTwoScore; i++)
+            {
+                tennisGame.AwardPoint(false);
+            }
+
+            Assert.Equal(score, tennisGame.Score);
         }
     }
 }
